28ac47b1463dbe76524137418313bf25ea45668d
Luke Biery <luke@bierysbargainbarn.com>
2017-03-20 13:03:30 -0400

    add favicon changer

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
+function change_favicon(img) {
+    var favicon = document.querySelector('link[rel="shortcut icon"]');
+
+    if (!favicon) {
+        favicon = document.createElement('link');
+        favicon.setAttribute('rel', 'shortcut icon');
+        var head = document.querySelector('head');
+        head.appendChild(favicon);
+    }
+
+
+    favicon.setAttribute('type', 'image/png');
+    favicon.setAttribute('href', img);
+}
+
 alert("test")
