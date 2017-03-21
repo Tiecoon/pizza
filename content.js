3b225a2331a9954a05861015721ab664bc1d9108
Kevin Zhan <kevin.w.zhan@gmail.com>
2017-03-21 12:28:47 -0400

    Disable new replacement code
    breaks everything for some reason.

--- content.js
+++ content.js
@@ -16,33 +16,33 @@ function change_favicon(img) {
 change_favicon(chrome.runtime.getURL("/favicon/favicon-32x32.png"));
 
 //"find and replace" only for body probably safer
-//document.body.innerHTML = document.body.innerHTML.replace(/Piazza/g, "Pizza");
-// document.body.innerHTML = document.body.innerHTML.replace(/piazza/g, "pizza");
+document.body.innerHTML = document.body.innerHTML.replace(/Piazza/g, "Pizza");
+document.body.innerHTML = document.body.innerHTML.replace(/piazza/g, "pizza");
 
 //new "find and replace" code
-var elements = document.getElementsByTagName('*');
+// var elements = document.getElementsByTagName('*');
 
-for (var i = 0; i < elements.length; i++) {
-	var element = elements[i];
+// for (var i = 0; i < elements.length; i++) {
+// 	var element = elements[i];
 
-	for (var j = 0; j < element.childNodes.length; j++) {
-		var node = element.childNodes[j];
+// 	for (var j = 0; j < element.childNodes.length; j++) {
+// 		var node = element.childNodes[j];
 
-		if (node.nodeType === 3) {
-			// uppercase
-			var text = node.nodeValue;
-			var replacedText = text.replace(/Piazza/g, "Pizza");
-			if (replacedText !== text)
-				element.replaceChild(document.createTextNode(replacedText), node);
+// 		if (node.nodeType === 3) {
+// 			// uppercase
+// 			var text = node.nodeValue;
+// 			var replacedText = text.replace(/Piazza/g, "Pizza");
+// 			if (replacedText !== text)
+// 				element.replaceChild(document.createTextNode(replacedText), node);
 			
-			// lowercase
-			text = node.nodeValue;
-			replacedText = text.replace(/piazza/g, "pizza");
-			if (replacedText !== text)
-				element.replaceChild(document.createTextNode(replacedText), node);
-		}
-	}
-}
+// 			// lowercase
+// 			text = node.nodeValue;
+// 			replacedText = text.replace(/piazza/g, "pizza");
+// 			if (replacedText !== text)
+// 				element.replaceChild(document.createTextNode(replacedText), node);
+// 		}
+// 	}
+// }
 
 
 //returns url for background image in appropriate format
