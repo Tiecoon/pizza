be43b6e715966adcc89b005f432d4bc55f750aee
Luke Biery <luke@bierysbargainbarn.com>
2017-03-21 12:30:39 -0400

    implement all basics

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 function change_favicon(img) {
+
 	var favicon = document.querySelector('link[rel="shortcut icon"]');
 
 	if (!favicon) {
@@ -16,8 +17,7 @@ function change_favicon(img) {
 change_favicon(chrome.runtime.getURL("/favicon/favicon-32x32.png"));
 
 //"find and replace" only for body probably safer
-document.body.innerHTML = document.body.innerHTML.replace(/Piazza/g, "Pizza");
-document.body.innerHTML = document.body.innerHTML.replace(/piazza/g, "pizza");
+//document.body.innerHTML = document.body.innerHTML.replace(/Piazza/g, "Pizza");
 
 //returns url for background image in appropriate format
 function convurl(dir){
@@ -26,3 +26,51 @@ function convurl(dir){
 }
 
 document.getElementById("classes_brand").style.backgroundImage = convurl("img/piazza_classes_logo_white_new.png");
+
+walk(document.body);
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bPiazza\b/g, "Pizza");
+	v = v.replace(/\bpiazza\b/g, "Pizza");
+
+	textNode.nodeValue = v;
+}
+
+var y = document.getElementsByTagName("span");
+var i;
+for (i = 0; i < y.length; i++) {
+    y[i].innerhtml = y[i].innerHTML.replace(/Piazza/g, "Pizza");
+    y[i].innerhtml = y[i].innerHTML.replace(/piazza/g, "pizza");
+}
